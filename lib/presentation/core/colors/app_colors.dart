278ce9491d9c4075754f9fc2c127b65ad1019019
KVeramendi <1611619@utp.edu.pe>
2023-01-03 00:44:31 -0500

    Se modificó el textfield y se terminó las tareas pendientes

---
+++ lib/presentation/core/colors/app_colors.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  static const Color primaryAppColor = Color(0xFFD9D9D9);
+  static const Color secondaryAppColor = Color(0xFF373A43);
+  static const Color tertiaryAppColor = Color(0xFFF9444F);
+  static const Color hintTextColor = Color(0xFF7B7D7D);
+}
--- lib/presentation/views/pages/auth/login/login_page.dart
+++ lib/presentation/views/pages/auth/login/login_page.dart
@@ -1,33 +1,75 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_chat_app/presentation/core/colors/app_colors.dart';
 import 'package:flutter_chat_app/presentation/core/shared_widgets/text_field_widget.dart';
+import 'package:flutter_chat_app/presentation/core/utils/validators.dart';
 
 class LoginPage extends StatelessWidget {
-
   static const routeName = 'login';
-  const LoginPage({ Key? key }) : super(key: key);
+  const LoginPage({Key? key}) : super(key: key);
+
   @override
   Widget build(BuildContext context) {
+    final GlobalKey<FormState> _formKey = GlobalKey();
     final TextEditingController emailController = TextEditingController();
     final TextEditingController passwordController = TextEditingController();
     return Scaffold(
-      body: Column(
-        children: [
-          Container(
-            width: 250,
-            height: 250,
-            color: Colors.red,
-          ),
-          const SizedBox(height:30),
-          TextFieldWidget(
-            controller: emailController, 
-            textValue: 'Email'
-          ),
-          TextFieldWidget(
-            controller: passwordController, 
-            textValue: 'Password'
-          ),
-        ],
+      body: Container(
+        padding: const EdgeInsets.all(30.0),
+        child: Column(
+          children: [
+            Container(
+              width: 250,
+              height: 250,
+              color: Colors.red,
+            ),
+            const SizedBox(height: 30),
+            Form(
+              key: _formKey,
+              child: Column(children: [
+                Row(
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children: [
+                    const Icon(
+                      Icons.email_rounded,
+                      size: 36.0,
+                      color: AppColors.primaryAppColor,
+                    ),
+                    const SizedBox(width: 10.0),
+                    Expanded(
+                      child: TextFieldWidget(
+                          controller: emailController,
+                          textValue: 'Email',
+                          validator: (value) =>
+                              Validators.validateEmail(value)),
+                    ),
+                  ],
+                ),
+                const SizedBox(height: 20.0),
+                Row(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    const Icon(
+                      Icons.lock_rounded,
+                      size: 36.0,
+                      color: AppColors.primaryAppColor,
+                    ),
+                    const SizedBox(width: 10.0),
+                    Expanded(
+                      child: TextFieldWidget(
+                          controller: passwordController,
+                          textValue: 'Password',
+                          validator: (value) =>
+                              Validators.validatePassword(value)),
+                    ),
+                  ],
+                ),
+              ]),
+              autovalidateMode: AutovalidateMode.onUserInteraction,
+            ),
+          ],
+        ),
       ),
+      backgroundColor: AppColors.secondaryAppColor,
     );
   }
 }
