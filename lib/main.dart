a3d02c7ccd87c3f207136d12c30ac6c7e31e6489
BrunoSDS <bruno@sdsharp.com>
2023-01-04 19:31:01 -0500

    feat: :sparkles: Chat page
    Chat page
    
    Ref: #003

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_chat_app/presentation/core/routes/routes.dart';
 import 'package:flutter_chat_app/presentation/views/pages/auth/login/login_page.dart';
+import 'package:flutter_chat_app/presentation/views/pages/chat/home/chat_home_page.dart';
 
 void main() => runApp(MyApp());
 
@@ -11,7 +12,7 @@ class MyApp extends StatelessWidget {
       debugShowCheckedModeBanner: false,
       title: 'Material App',
       routes: appRoutes,
-      home: LoginPage(),
+      home: const ChatHomePage(),
     );
   }
 }
--- lib/presentation/core/shared_widgets/button_submit_widget.dart
+++ lib/presentation/core/shared_widgets/button_submit_widget.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_chat_app/presentation/core/colors/app_colors.dart';
 
 class ButtonSubmit extends StatefulWidget {
   final VoidCallback? function;
@@ -13,7 +14,19 @@ class _ButtonSubmitState extends State<ButtonSubmit> {
   Widget build(BuildContext context) {
     return TextButton(
       onPressed: widget.function, 
-      child: Text('Click me')
+      style: ButtonStyle(
+        overlayColor: MaterialStateProperty.all<Color>(AppColors.secondaryAppColor.withOpacity(0.1)),
+        backgroundColor: MaterialStateProperty.all<Color>(AppColors.tertiaryAppColor),
+        minimumSize: MaterialStateProperty.all<Size>(const Size(300, 50)),
+        maximumSize: MaterialStateProperty.all<Size>(const Size(300, 50)),
+        shape: MaterialStateProperty.all<RoundedRectangleBorder>(
+          RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(25.0),
+          )
+        )
+        //padding: MaterialStateProperty.all(const EdgeInsets.symmetric(horizontal: 80,vertical: 10)),
+      ),
+      child: const Text('Login',style: TextStyle(color: Colors.white))
     );
   }
 }
