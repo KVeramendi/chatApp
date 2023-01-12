0cdf3d21d9051ad7c6286ed6aa3bda93d60ee626
KVeramendi <1611619@utp.edu.pe>
2023-01-11 19:25:00 -0500

    feat: :art: Add Splash Page
    Se agregó la página Splash para el login

---
+++ lib/presentation/views/pages/splash/splash_page.dart
@@ -0,0 +1,41 @@
+import 'package:after_layout/after_layout.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_chat_app/data/datasources/local/auth_local.dart';
+import 'package:flutter_chat_app/presentation/logic/services/socket/socket_service.dart';
+import 'package:flutter_chat_app/presentation/views/pages/auth/login/login_page.dart';
+import 'package:flutter_chat_app/presentation/views/pages/chat/home/chat_home_page.dart';
+import 'package:provider/provider.dart';
+
+class SplashPage extends StatefulWidget {
+  const SplashPage({Key? key}) : super(key: key);
+
+  @override
+  State<SplashPage> createState() => _SplashPageState();
+}
+
+class _SplashPageState extends State<SplashPage> with AfterLayoutMixin {
+  @override
+  void afterFirstLayout(BuildContext context) {
+    _check();
+  }
+
+  _check() async {
+    final socketService = Provider.of<SocketService>(context, listen: false);
+    final String? token = await Auth.instance.accessToken;
+    if (token != null) {
+      socketService.connect();
+      Navigator.pushReplacementNamed(context, ChatHomePage.routeName);
+    } else {
+      Navigator.pushReplacementNamed(context, LoginPage.routeName);
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: LinearProgressIndicator(),
+      ),
+    );
+  }
+}
